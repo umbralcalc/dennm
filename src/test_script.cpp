9b15620409c9c4b72132947a0aac501a20d0d245
umbralcalc <umbralcalc@gmail.com>
2023-10-13 14:06:08 +0100

    messing around with some functions

--- src/test_script.cpp
+++ src/test_script.cpp
@@ -1,7 +1,15 @@
 #include <torch/torch.h>
 
+
+torch::Tensor stateProbsUpdate(torch::Tensor stateProbs, torch::Tensor times) {
+    return 2 * stateProbs;
+}
+
 int main() {
     torch::Tensor tensor = torch::rand({2, 2});
+    for (int i = 0; i <= 10; i++) {
+        tensor = stateProbsUpdate(tensor, tensor);
+    }
     std::cout << tensor << std::endl;
     return 0;
 }
