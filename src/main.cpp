2b3ab6de4a46b8b4b3be3e680d6b36d8995b911d
umbralcalc <umbralcalc@gmail.com>
2023-10-18 18:37:17 +0100

    added code for the third order system - ready for test drive

--- src/main.cpp
+++ src/main.cpp
@@ -16,7 +16,7 @@ public:
 
     void update(Dimensions dims, torch::Tensor conditionalProbs) {
         auto numberOfTimesteps = double(probs.size(axes.time));
-        probs += (
+        torch::Tensor nextProbs = (
             (dims.spacestep / numberOfTimesteps) * torch::tensordot(
                 probs, 
                 conditionalProbs, 
@@ -24,6 +24,7 @@ public:
                 {axes.space + 2, axes.time + 2}
             ) - probs
         );
+        probs = torch::cat({probs, nextProbs}, axes.time);
     }
 
     torch::Tensor getProbs() const {
@@ -44,8 +45,18 @@ public:
         conditionalProbs(initialConditionalProbs),
         axes(axesSetup) {}
 
-    void update(Dimensions dims) {
+    void update(Dimensions dims, torch::Tensor twiceConditionalProbs) {
         SecondOrderProbsState::update(dims, conditionalProbs);
+        auto numberOfTimesteps = double(conditionalProbs.size(axes.time));
+        torch::Tensor nextConditionalProbs = (
+            (dims.spacestep / numberOfTimesteps) * torch::tensordot(
+                conditionalProbs, 
+                twiceConditionalProbs, 
+                {axes.space, axes.time}, 
+                {axes.space + 2, axes.time + 2}
+            ) - conditionalProbs
+        );
+        conditionalProbs = torch::cat({conditionalProbs, nextConditionalProbs}, axes.time);
     }
 
     torch::Tensor getProbs() const {
@@ -70,7 +81,7 @@ int main() {
     torch::Tensor tensor = torch::rand({2, 2});
     ThirdOrderProbsState state(tensor, tensor, axes);
     for (int i = 0; i <= 10; i++) {
-        state.update(dims);
+        state.update(dims, tensor);
     }
     std::cout << state.getProbs() << std::endl;
     return 0;
