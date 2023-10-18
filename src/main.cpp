a0821bb047c771fa27ea97ad9f8f5cd99864826a
umbralcalc <umbralcalc@gmail.com>
2023-10-18 11:15:20 +0100

    advancing the code definitions of steps

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,77 @@
 #include <torch/torch.h>
 
+struct Dimensions {
+    double spacestep;
+};
 
-torch::Tensor stateProbsUpdate(torch::Tensor stateProbs, torch::Tensor times) {
-    return 2 * stateProbs;
-}
+struct Axes {
+    int space;
+    int time;
+};
+
+class SecondOrderProbsState {
+public:
+    SecondOrderProbsState(torch::Tensor initialProbs, Axes axesSetup) : 
+        probs(initialProbs), axes(axesSetup) {}
+
+    void update(Dimensions dims, torch::Tensor conditionalProbs) {
+        auto numberOfTimesteps = double(probs.size(axes.time));
+        probs += (
+            (dims.spacestep / numberOfTimesteps) * torch::tensordot(
+                probs, 
+                conditionalProbs, 
+                {axes.space, axes.time}, 
+                {axes.space + 2, axes.time + 2}
+            ) - probs
+        );
+    }
+
+    torch::Tensor getProbs() const {
+        return probs;
+    }
+private:
+    torch::Tensor probs;
+    const Axes axes;
+};
+
+class ThirdOrderProbsState : public SecondOrderProbsState {
+public:
+    ThirdOrderProbsState(
+        torch::Tensor initialProbs, 
+        torch::Tensor initialConditionalProbs,
+        Axes axesSetup
+    ) : SecondOrderProbsState(initialProbs, axesSetup), 
+        conditionalProbs(initialConditionalProbs),
+        axes(axesSetup) {}
+
+    void update(Dimensions dims) {
+        SecondOrderProbsState::update(dims, conditionalProbs);
+    }
+
+    torch::Tensor getProbs() const {
+        return SecondOrderProbsState::getProbs();
+    }
+
+    torch::Tensor getConditionalProbs() const {
+        return conditionalProbs;
+    }
+private:
+    torch::Tensor conditionalProbs;
+    const Axes axes;
+};
+
 
 int main() {
+    Dimensions dims;
+    dims.spacestep = 1.0;
+    Axes axes;
+    axes.space = 0;
+    axes.time = 1;
     torch::Tensor tensor = torch::rand({2, 2});
+    ThirdOrderProbsState state(tensor, tensor, axes);
     for (int i = 0; i <= 10; i++) {
-        tensor = stateProbsUpdate(tensor, tensor);
+        state.update(dims);
     }
-    std::cout << tensor << std::endl;
+    std::cout << state.getProbs() << std::endl;
     return 0;
 }
